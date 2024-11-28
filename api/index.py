3dae141bb52fb80d20b455063c00372af197b41e
Leonardo Messa Wagner <leonardowagner22@outlook.com>
2024-11-27 23:07:14 -0300

    1 update rota raiz

--- api/index.py
+++ api/index.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import os
+import cv2
+from fer import FER
+import numpy as np
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def hello():
+    return "API para Upload e Processamento de Vídeo"
 
 @app.route('/about')
 def about():
